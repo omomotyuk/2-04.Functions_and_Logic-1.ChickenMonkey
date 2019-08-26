9bfb4728cfb0aa764fce3150d7df26fc72957e5a
Alex <o.a.momotyuk@gmail.com>
2019-08-26 15:37:13 -0500

    Practice is done.

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+Write a program that prints the numbers from 1 to 100. But for multiples of five (5, 10, 15, etc.) print "Chicken" instead of the number and for the multiples of seven (7, 14, 21, etc.) print "Monkey". For numbers which are multiples of both five and seven print "ChickenMonkey".
+
+To determine if a number can be evenly divided by 5 or 7, use the JavaScript remainder operator.
+
+You can start with this simpler version that outputs even numbers to the console and ignores odd numbers. You need to refactor this code to meet the requirements above.
+*/
+
+/*
+for ( let currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
+    // Divide the current number by 2, and check if the remainder is 0
+    if (currentNumber % 2 === 0) {
+        console.log( currentNumber ) // Only 2, 4, 6 will appear
+    }
+}
+*/
+
+for ( var i = 1; i <= 100; i++ ) {
+
+    let Chicken = i % 5;
+    let Monkey = i % 7;
+
+    if( Chicken === 0 && Monkey === 0 ) { 
+        console.log( "ChickenMonkey" );
+    } else if ( !Chicken ) {
+        console.log( "Chicken" );
+    } else if ( !Monkey ) {
+        console.log( "Monkey" );       
+    } else {
+        console.log( i );       
+    }
+}
